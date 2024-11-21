917b21ebde1f48cf5af5057b94ca94d53548e8a7
phamnam2003 <namphamhai7@gmail.com>
2024-11-21 14:58:09 +0700

    feat(db): begin transaction and rollback query

--- internal/database/sqlc/store.go
+++ internal/database/sqlc/store.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -19,4 +22,27 @@ func NewStore(db *pgxpool.Pool) Store {
 		Queries:        New(db),
 		connectionPool: db,
 	}
+}
+
+func (sqlStore *SQLStore) execTX(ctx context.Context, fn func(*Queries) error) error {
+	transaction, err := sqlStore.connectionPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	queries := New(transaction)
+	err = fn(queries)
+	if err != nil {
+		if errRollback := transaction.Rollback(ctx); errRollback != nil {
+			return fmt.Errorf("[ERROR - Rollback]: %s", errRollback)
+		}
+		return fmt.Errorf("[ERROR - Unknow]: %s", err)
+	}
+
+	errCommit := transaction.Commit(ctx)
+	if errCommit != nil {
+		return fmt.Errorf("[ERROR - Commit]: %s", errCommit)
+	}
+
+	return nil
 }
